95f9f295423d2064a235a8b0492f1ce09943b29f
parker mccurley <parker@decentcrypto.com>
2018-08-18 11:14:47 -0400

    add httprouter and endpoints

--- main.go
+++ main.go
@@ -2,17 +2,29 @@ package main
 
 import (
   "fmt"
-  "html"
   "log"
   "net/http"
+
+  "github.com/julienschmidt/httprouter"
 )
 
+func Index(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+  fmt.Fprint(w, "Welcome!\n")
+}
+
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+  fmt.Fprintf(w, "Hello, %s!\n", ps.ByName("name"))
+}
+
 func main() {
-	fmt.Println("Hello, world!")
+  router := httprouter.New()
 
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-  })
+  router.GET("/", Index)
+  router.GET("/hello/:name", Hello)
 
-  log.Fatal(http.ListenAndServe(":8080", nil))
+//   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+//     fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+//   })
+
+  log.Fatal(http.ListenAndServe(":8080", router))
 }
